2547a81040d36d0f64968f2f5fae001c23610a35
anubhav <anubhavthrilling24@gmail.com>
2022-05-22 16:33:35 +0530

    Init ts

--- server/index.js
+++
@@ -1,5 +0,0 @@
-import "./common/env";
-import Server from "./common/server";
-import routes from "./routes";
-
-export default new Server().router(routes).connectDB().listen(process.env.PORT);
--- server/routes.js
+++
@@ -1,5 +0,0 @@
-import userRouter from "./api/controllers/user/router";
-
-export default function routes(app) {
-  app.use("/api/v1/user", userRouter);
-}
