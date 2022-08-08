5f7b647833645d1ecaeb7f5c6e2828081f8ed4bd
anubhav <anubhavthrilling24@gmail.com>
2022-08-08 21:12:17 +0530

    fix(Boat): Add and update boat images

--- server/api/controllers/naav/naavController.ts
+++ server/api/controllers/naav/naavController.ts
@@ -29,7 +29,7 @@ export class NaavController {
     try {
       if (req.file) {
         const result = await cloudinary.uploader.upload(req.file.path);
-        req.body.picture = result.secure_url;
+        req.body.pictures = [result.secure_url];
       }
       const userId = res.locals?.user?.data.data
         ? res.locals?.user?.data.data
@@ -46,19 +46,28 @@ export class NaavController {
   async update(req: Request, res: Response) {
     try {
       const { id } = req.params;
-
+      let image = '';
       if (req.file) {
         const result = await cloudinary.uploader.upload(req.file.path);
-        req.body.picture = result.secure_url;
+        image = result.secure_url;
       }
       const user = res.locals?.user?.data.data
         ? res.locals?.user?.data.data
         : res.locals?.user?.data;
 
       req.body.user = user;
-      const naav = await Naav.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
+      const naav = await Naav.findByIdAndUpdate(
+        id,
+        {
+          $set: req.body,
+          $push: {
+            pictures: image,
+          },
+        },
+        {
+          new: true,
+        }
+      );
       return res.json(naav);
     } catch (error) {
       return res.status(400).send({ message: 'Unable to update naav' });
