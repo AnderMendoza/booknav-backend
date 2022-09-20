fd10a6ce093c69486816feac9ae2f10e3f031372
anubhav <anubhavthrilling24@gmail.com>
2022-09-20 18:24:26 +0530

    fix(Bank): Admin bank get with id

--- server/api/controllers/bank/bankRouter.ts
+++ server/api/controllers/bank/bankRouter.ts
@@ -2,9 +2,11 @@ import * as express from 'express';
 import isNaavik from '../../middlewares/isNaavik';
 import isAuthenticated from '../../middlewares/isAuthenticated';
 import bankController from './bankController';
+import isAdmin from '../../middlewares/isAdmin';
 
 export default express
   .Router()
+  .get('/:id', isAuthenticated, isAdmin, bankController.getById)
   .put('/:id', isAuthenticated, isNaavik, bankController.update)
   .delete('/:id', isAuthenticated, bankController.delete)
   .get('/', isAuthenticated, bankController.get)
